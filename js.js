da846a80afd320032c719eee6fb7b149c04408fb
arjunindia <arjun8107@gmail.com>
2019-11-24 21:22:30 +0530

    l

---
+++ js.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('*').click(function() {
+		/* Act on the event */
+		
+	});
+    $('#1').click(function() {
+    	/* Act on the event */
+    	$('#i11').delay(500).toggleClass('yellower');;
+    });
+});
